f55dc70e9bf73a84a8d639cd8e9c94b03429d65b
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-08 00:12:35 +0530

    minor change in spec and views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
   		visit '/static_pages/home'
   		expect(page).to have_content('Sample App')
   	end
+
+  	it "should have title 'Home'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Ruby on rails sample app | Home")
+  	end
   end
 
   describe "Help page" do
@@ -13,12 +18,22 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+
+  	it "should have title 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Ruby on rails sample app | Help")
+  	end
   end
 
   describe "About page" do
   	it "should have content 'About App'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About App')
+  	end
+
+  	it "should have title 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("Ruby on rails sample app | About App")
   	end
   end
 end
