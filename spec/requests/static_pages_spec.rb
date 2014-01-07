5c2c05eb1d3c2d7630029a085fcfcd642428fb2a
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-07 23:42:29 +0530

    minor change to spec file and updated routes, controller and view

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
   	it "should have content 'Sample App'" do
-  		visit 'static_pages/home'
-  		expect(page).to_have_content('Sample App')
+  		visit '/static_pages/home'
+  		expect(page).to have_content('Sample App')
   	end
   end
 
   describe "Help page" do
   	it "should have content 'Help'" do
-  		visit 'static_pages/help'
-  		expect(page).to_have_content('Help')
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
   	end
   end
 
   describe "About page" do
   	it "should have content 'About App'" do
-  		visit 'static_pages/about'
-  		expect(page).to_have_content('About')
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About App')
   	end
   end
 end
