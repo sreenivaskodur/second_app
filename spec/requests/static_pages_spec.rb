6e028cb5ff484be668a4db5c924122210b59cfa0
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-07 22:09:59 +0530

    updated spec and changed view for home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,6 +5,20 @@ describe "StaticPages" do
   	it "should have content 'Sample App'" do
   		visit 'static_pages/home'
   		expect(page).to_have_content('Sample App')
-  	do
+  	end
+  end
+
+  describe "Help page" do
+  	it "should have content 'Help'" do
+  		visit 'static_pages/help'
+  		expect(page).to_have_content('Help')
+  	end
+  end
+
+  describe "About page" do
+  	it "should have content 'About App'" do
+  		visit 'static_pages/about'
+  		expect(page).to_have_content('About')
+  	end
   end
 end
