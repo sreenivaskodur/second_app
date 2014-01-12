4cd1e273b306c80867e8c8e5f0501554b2d26022
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-12 23:38:37 +0530

    named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,48 +3,48 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
   	it "should have content 'Sample App'" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_content('Sample App')
   	end
 
   	it "should have title 'Home'" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_title("Ruby on rails sample app | Home")
   	end
   end
 
   describe "Help page" do
   	it "should have content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
 
   	it "should have title 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_title("Ruby on rails sample app | Help")
   	end
   end
 
   describe "About page" do
   	it "should have content 'About App'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About App')
   	end
 
   	it "should have title 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_title("Ruby on rails sample app | About App")
   	end
   end
 
   describe "Contact page" do
   	it "should have content 'Contact Details'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_content("Contact Details")
   	end
 
   	it "should have title 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_title("Ruby on rails sample app | Contact")
   	end
   end
