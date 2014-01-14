8495ba8ccc126a8391b5bc4b8a1dbafeb2585aed
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-15 00:35:14 +0530

    adding validations on name field

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before{@user=User.new(name:"Example Name",email:"email@example.com")}
+
+	subject{@user}
+
+	it{should respond_to(:name)}
+	it{should respond_to(:email)}
+	it{should be_valid}
+
+	describe "set name to empty string and the result should be invalid" do
+		before{@user.name=""}
+		it{should_not be_valid}
+	end
+
+	describe "set name to length beyond 50 chars and result should be invalid" do
+		before{@user.name="a" * 51}
+		it{should_not be_valid}
+	end
 end
