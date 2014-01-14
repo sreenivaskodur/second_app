9fff8f19267f4a283b18ae5b9ba75a7312d95138
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-15 01:14:18 +0530

    adding validations on email field

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,30 @@ describe User do
 		before{@user.name="a" * 51}
 		it{should_not be_valid}
 	end
+
+	describe "the result should be invalid for these email-Id's " do
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+        addresses.each do |invalid_id|
+        	before{@user.email=invalid_id}
+        	it{should_not be_valid}
+        end
+	end
+
+	describe "the result should be valid for these email-Id's " do
+		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+		addresses.each do |valid_id|
+			before{@user.email=valid_id}
+			it{should be_valid}
+		end
+	end
+
+	describe "creating a entry with duplicate email-Id and the result should be not valid" do
+		before do
+			email_with_dup_email = @user.dup
+			email_with_dup_email.save
+		end
+
+		it{should_not be_valid}
+	end
 end
