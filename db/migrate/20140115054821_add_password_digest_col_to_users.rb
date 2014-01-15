cce1f4cdb3ea2801424c2e88c4e2a219eac74534
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-15 14:10:51 +0530

    adding authentication for password field

---
+++ db/migrate/20140115054821_add_password_digest_col_to_users.rb
@@ -0,0 +1,5 @@
+class AddPasswordDigestColToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 
 describe User do
-	before{@user=User.new(name:"Example Name",email:"email@example.com")}
+	before do
+		@user=User.new(name:"Example Name",email:"email@example.com",
+		password:"passwd",password_confirmation:"passwd")
+	end
 
 	subject{@user}
 
 	it{should respond_to(:name)}
 	it{should respond_to(:email)}
+	it{should respond_to(:password_digest)}
+	it{should respond_to(:password)}
+	it{should respond_to(:password_confirmation)}
+
 	it{should be_valid}
+
+	describe "password test using authenticate method" do
+		before{@user.save}
+		let(:curr_user){User.find_by(email:@user.email)}
+
+		describe "authenticate successfully when password matches" do
+			it{should eq curr_user.authenticate(@user.password)}
+		end
+
+		describe "authentication failure when there is a mismatch" do
+			let(:user_with_invalid_passwd){curr_user.authenticate("invalid password")}
+			it{should_not eq user_with_invalid_passwd}
+		end
+	end
 
 	describe "set name to empty string and the result should be invalid" do
 		before{@user.name=""}
@@ -42,6 +63,21 @@ describe User do
 			email_with_dup_email.save
 		end
 
+		it{should_not be_valid}
+	end
+
+	describe "when password is empty reuslt should be not valid" do
+		before do
+			@user = User.new(name:"example",email:"user@example.com",
+				password:"",password_confirmation:"")
+		end
+		it{should_not be_valid}
+	end
+
+	describe "when there is mismatch in password, result should be not valid" do
+		before do
+			@user.password_confirmation = "something else"
+		end
 		it{should_not be_valid}
 	end
 end
