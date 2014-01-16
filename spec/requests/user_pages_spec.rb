cfe66ba7c7c08bf3e91c76ed6df798d98f5107f3
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-16 23:28:52 +0530

    html form for signup page, using factory girl instead of active record in spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,23 @@ describe "User Pages" do
   	it{should have_content("Sign Up")}
   	it{should have_title(full_title("Sign Up"))}
   end
+
+  describe "profile page spec using active record" do
+  	before do
+  		@user = User.create(name:"Tester",email:"tester@testing.com",
+  			password:"temporary",password_confirmation:"temporary")
+  		visit user_path(@user.id)
+  	end
+
+  	it{should have_content(@user.name)}
+  	it{should have_title(@user.name)}
+  end
+
+  describe "profile page spec using factory girl" do
+  	let(:user) {FactoryGirl.create(:user)}
+  	before{visit user_path(user)}
+
+  	it{should have_content(user.name)}
+  	it{should have_title(user.name)}
+  end
 end
