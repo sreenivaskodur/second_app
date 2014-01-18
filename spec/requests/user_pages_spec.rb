02e895cad2e4f31b5e41b504e6046b0416ebbf12
Sreenivas Kodur <sreenivas.kodur@gmail.com>
2014-01-18 12:14:39 +0530

    spec for filling signup from

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,4 +27,27 @@ describe "User Pages" do
   	it{should have_content(user.name)}
   	it{should have_title(user.name)}
   end
+
+  describe "Testing signup page" do
+  	before{visit signup_path}
+
+  	let(:submit){"Create my account"}
+
+  	describe "Invalid signup request" do
+  		it "should not create an account with empty fields" do
+  			expect{click submit}.not_to change(User,:count)
+  		end
+  	end
+
+  	describe "Valid signup request would increase number of users by 1" do
+  		before do
+  			fill_in "Name",with:"Example Name"
+  			fill_in "Email",with:"user@example.com"
+  			fill_in "Password",with:"testing"
+  			fill_in "Confirm Password",with:"testing"
+  		end
+
+  		expect{click submit}.to change(User,:count).by(1)
+  	end
+  end
 end
