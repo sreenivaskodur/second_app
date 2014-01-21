28bf6124f04630ad909b3d9fe55095045a4181f9
sreenivaskodur <sreenivas.kodur@gmail.com>
2014-01-22 00:05:36 +0530

    signin form (action incomplete for successful login attempt). Added few more specs for signin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:session][:email])
+        if user && user.authenticate(params[:user][:password])
+        else
+            flash[:error].now = "Invalid email/password combination"
+            render 'new'
+        end
     end
 
     def destroy
