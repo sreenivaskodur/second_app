af0bdbc5486fd78adf127847754e9f743fb32f38
sreenivaskodur <sreenivas.kodur@gmail.com>
2014-01-22 23:53:55 +0530

    added action for successful signin. Added cookie for remembering session (left with updating links in views e.t.c)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email])
         if user && user.authenticate(params[:user][:password])
+            sign_in user
+            redirect_to user
         else
             flash[:error].now = "Invalid email/password combination"
             render 'new'
